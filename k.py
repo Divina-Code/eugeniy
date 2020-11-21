f79e00973b74efca57f3044760d6c07e12b02bea
Eugeniy0 <74458413+Eugeniy0@users.noreply.github.com>
2020-11-21 17:00:07 +0300

    Update k.py

--- k.py
+++ k.py
@@ -1,8 +1,8 @@
 
     from random import randit
->>> word="австралия"
->>> leter=input("введите букву") 
->>> if leter in word
->>>    print ("есть такая буква")
+word="австралия"
+leter=input("введите букву") 
+if leter in word
+    print ("есть такая буква")
  if leter not in the word
     print("нет такой буквы")
