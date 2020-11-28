be3edd69b8b37ec6c078b84b5a7d0b547fe85ac2
Eugeniy0 <74458413+Eugeniy0@users.noreply.github.com>
2020-11-28 17:18:20 +0300

    Add files via upload

--- ынко.py
+++ ынко.py
@@ -9,8 +9,21 @@ lives = 10
 
 letters=[]
 while game :
-   leter=input("введите букву или слово: ")
+   print ()
+   print()
    print("*"+"___*"*len(word))
+   leter=input("введите букву или слово: ")
+   if len(letter)<1
+      print("Надо ввести букву или слово")
+
+      elif len(letter)==1
+         if letter==word:
+            if letter not in letters:
+             print("Есть такая буква")
+             letters.append(letter)
+          else:
+              print("Ты уже говорил эту букву")
+              lives=lives-1
    if leter==word:
       print("ТЫ ПОБЕДИЛ game over")
       game=False
