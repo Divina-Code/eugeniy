90999f2356e13f3a4354c7d802f3205b9503c3cc
Eugeniy0 <74458413+Eugeniy0@users.noreply.github.com>
2020-12-12 18:10:49 +0300

    Update k.py

--- k.py
+++ k.py
@@ -1,10 +1,39 @@
-from random import randit
+from random import randint
 
-word = "австралия"
-leter = input("введите букву")
 
-if leter in word:
-    print ("есть такая буква")
-    
-if leter not in the word:
-    print("нет такой буквы")
+
+
+print("добро пожаловать в игру 21")
+n = int(input("Сколько игроков будет?\t"))  
+print()
+
+
+players = [] 
+for i in range(n):  
+    name = input("Введите имя " +str(i+1)+"-го игрока:\t" )
+    players.append(name)
+
+print("\nИГРОКИ: ", players)
+
+points = [] 
+for j in range(n):
+    random_points = randint(1, 10)
+    points.append(random_points)
+    print(players[j], "Ваш счёт:", random_points)
+
+print("\nОЧКИ: ",points)
+
+game = True 
+while game:
+    for a in range(n):
+        answer = input(players[a] + ' будете брать карту? [ДА\НЕТ]')
+
+        answer = answer.upper() 
+        answer = answer.strip()  
+
+        if answer == "ДА":
+            print("Вы ответили ", answer)
+        elif answer == "НЕТ":
+            print("Вы ответили ", answer)
+        else:
+            print("Не понял твоего ответа, принимаю только 'ДА' или 'НЕТ' ")
